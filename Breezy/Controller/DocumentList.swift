c1b72f22aac59c6382bf518132d39545931a5988
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2018-02-04 17:59:06 -0500

    added more NS FRC boilerplate

--- Breezy/Controller/DocumentList.swift
+++ Breezy/Controller/DocumentList.swift
@@ -11,20 +11,25 @@ import CoreData
 
 class DocumentList: UIViewController, NSFetchedResultsControllerDelegate, UITableViewDataSource {
   
+  // TODO: call new document command on every launch
+  
+  // MARK: Properties
+  @IBOutlet weak var tableView: UITableView!
+  
   var frc: NSFetchedResultsController<Document> {
-    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     let fetchRequest: NSFetchRequest<Document> = Document.fetchRequest()
     fetchRequest.sortDescriptors = [NSSortDescriptor(key: "creation", ascending: true)]
-    
     let fetchedResultsController = NSFetchedResultsController<Document>(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-    
     return fetchedResultsController
   }
   
+  // MARK: App Start
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  // MARK: Tableview Methods
   
   func configure(_ cell: DocumentListCell, with object: Document) {
     cell.configure(with: object)
@@ -46,6 +51,30 @@ class DocumentList: UIViewController, NSFetchedResultsControllerDelegate, UITabl
     let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell") as! DocumentListCell
     configure(cell, with: frc.object(at: indexPath))
     return cell
+  }
+  
+  // MARK: NS FRC Methods
+  
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.endUpdates()
+  }
+  
+  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.beginUpdates()
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    
+    switch type {
+    case .update:
+      // update table
+    case .move:
+      // update table
+    case .delete:
+      // update table
+    case .insert:
+      // update table
+    }
   }
   
 }
